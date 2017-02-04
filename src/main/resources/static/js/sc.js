1c9c1fae162c9be8eec7f71fe31665d6c7ac45f3
1element <1element@users.noreply.github.com>
2017-02-04 17:44:10 +0100

    Optimized image refresh for liveview page

--- src/main/resources/static/js/sc.js
+++ src/main/resources/static/js/sc.js
@@ -3,25 +3,48 @@ var SurveillanceCenter = {
   CSS_TRIGGER_REFRESH_LIVEVIEW: '.js-refresh-liveview',
   CSS_TRIGGER_CAMERA_FALLBACK: '.js-camera-fallback',
   CSS_LIVEVIEW_CONTAINER: '#liveview-container',
+  CSS_LIVEVIEW_CONTAINER_LOADER: '#liveview-container-loader',
+  LIVEVIEW_CONTAINER_ID: 'liveview-container',
+  LIVEVIEW_CONTAINER_LOADER_ID: 'liveview-container-loader',
 
+  /**
+   * Initialize all listeners.
+   */
   init: function() {
     this.addRefreshLiveviewListener();
     this.addRotationListener();
     this.addCameraFallbackImagesListener();
   },
 
+  /**
+   * Refresh liveview image on click.
+   */
   addRefreshLiveviewListener: function() {
     var that = this;
     $(this.CSS_TRIGGER_REFRESH_LIVEVIEW).on('click', function() {
       var button = this;
       $(button).addClass('active');
+      $(button).prop('disabled', true);
       $.ajax({
         url: $(this).data('url')
       }).done(function(data) {
-        $(that.CSS_LIVEVIEW_CONTAINER).replaceWith(data);
-        $(button).removeClass('active');
-        that.rotateImages();
+        // load result in hidden container
+        $(that.CSS_LIVEVIEW_CONTAINER_LOADER).append(data);
         that.addCameraFallbackImagesListener();
+        // wait for image(s) to be loaded
+        $(that.CSS_LIVEVIEW_CONTAINER_LOADER).imagesLoaded(function() {
+          // move content to visible container
+          $(that.CSS_LIVEVIEW_CONTAINER).replaceWith($(that.CSS_LIVEVIEW_CONTAINER_LOADER));
+          $(that.CSS_LIVEVIEW_CONTAINER_LOADER).attr('id', that.LIVEVIEW_CONTAINER_ID);
+          $(that.CSS_LIVEVIEW_CONTAINER).removeClass('hidden');
+          // create a new hidden loader container for next request
+          $(that.CSS_LIVEVIEW_CONTAINER).after($('<div/>').attr('id', that.LIVEVIEW_CONTAINER_LOADER_ID).addClass('hidden'));
+          // rotate image if necessary
+          that.rotateImages();
+          // activate refresh button again
+          $(button).prop('disabled', false);
+          $(button).removeClass('active');
+        });
       });
     });
   },
@@ -39,6 +62,9 @@ var SurveillanceCenter = {
     });
   },
 
+  /**
+   * Listener to invoke image rotation.
+   */
   addRotationListener: function() {
     if ($('[data-rotate]').length == 0) {
       return;
@@ -51,6 +77,10 @@ var SurveillanceCenter = {
     });
   },
 
+  /**
+   * Rotate images.
+   * @private
+   */
   rotateImages: function() {
     $('[data-rotate]').each(function() {
       var rotateValue = $(this).data('rotate');
@@ -68,6 +98,9 @@ var SurveillanceCenter = {
 
 };
 
+/**
+ * Initialize SurveillanceCenter on dom ready.
+ */
 $(document).ready(function() {
   SurveillanceCenter.init();
 });
