7e24c51618754ab964996a56ee10d6b68038cde2
1element <1element@users.noreply.github.com>
2017-04-15 14:09:23 +0200

    Import statement unified

--- src/test/java/com/github/_1element/sc/SurveillanceCenterApplicationTests.java
+++ src/test/java/com/github/_1element/sc/SurveillanceCenterApplicationTests.java
@@ -1,12 +1,13 @@
 package com.github._1element.sc;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertTrue;
 
 import java.util.Properties;
 
@@ -18,7 +19,7 @@ public class SurveillanceCenterApplicationTests {
   @Test
   public void testContextLoads() throws Exception {
     Properties properties = PropertiesLoaderUtils.loadAllProperties("application.properties");
-    Assert.assertTrue(properties.size() > 0);
+    assertTrue(properties.size() > 0);
   }
 
 }
