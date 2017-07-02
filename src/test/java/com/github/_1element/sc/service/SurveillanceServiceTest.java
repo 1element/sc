027d132b90b87e458214c3d23504aa9d54dc7646
1element <1element@users.noreply.github.com>
2017-07-02 12:57:21 +0200

    Some unit test for surveillance service class added

--- src/test/java/com/github/_1element/sc/service/SurveillanceServiceTest.java
+++ src/test/java/com/github/_1element/sc/service/SurveillanceServiceTest.java
@@ -1,16 +1,23 @@
 package com.github._1element.sc.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -34,12 +41,26 @@ public class SurveillanceServiceTest {
 
   @Autowired
   private SurveillanceService surveillanceService;
+  
+  private static Camera camera1;
+  
+  private static Camera camera2;
+  
+  private static LocalDateTime time1;
+  
+  private static LocalDateTime time3;
+  
+  private static boolean fixturesCreated = false;
+  
+  @Before
+  public void setUp() throws Exception {
+    if (fixturesCreated) {
+      return;
+    }
 
-  @Test
-  public void testGetImagesCameraSummary() throws Exception {
     // test fixtures for testcamera1
-    Camera camera1 = cameraRepository.findById("testcamera1");
-    LocalDateTime time1 = LocalDateTime.of(2017, Month.JUNE, 2, 11, 18, 42);
+    camera1 = cameraRepository.findById("testcamera1");
+    time1 = LocalDateTime.of(2017, Month.JUNE, 2, 11, 18, 42);
     SurveillanceImage image1 = new SurveillanceImage("test-file1.jpg", camera1.getId(), time1);
     imageRepository.save(image1);
 
@@ -48,12 +69,17 @@ public class SurveillanceServiceTest {
     imageRepository.save(image2);
 
     // test fixtures for testcamera2
-    Camera camera2 = cameraRepository.findById("testcamera2");
-    LocalDateTime time3 = LocalDateTime.of(2017, Month.JUNE, 1, 8, 18, 44);
+    camera2 = cameraRepository.findById("testcamera2");
+    time3 = LocalDateTime.of(2017, Month.JUNE, 1, 8, 18, 44);
     SurveillanceImage image3 = new SurveillanceImage("test-file3.jpg", camera2.getId(), time3);
     imageRepository.save(image3);
+    
+    fixturesCreated = true;
+  }
 
-    // expectations
+  @Test
+  public void testGetImagesCameraSummary() throws Exception {
+    // expected results
     ImagesCameraSummaryResult expectedResultCamera1 = new ImagesCameraSummaryResult(camera1, 2L, time1);
     ImagesCameraSummaryResult expectedResultCamera2 = new ImagesCameraSummaryResult(camera2, 1L, time3);
     ImagesCameraSummaryResult expectedResultCamera3 = new ImagesCameraSummaryResult(cameraRepository.findById("testcamera3"), 0L, null);
@@ -66,6 +92,33 @@ public class SurveillanceServiceTest {
     assertTrue(result.stream().anyMatch(e -> expectedResultCamera1.equals(e)));
     assertTrue(result.stream().anyMatch(e -> expectedResultCamera2.equals(e)));
     assertTrue(result.stream().anyMatch(e -> expectedResultCamera3.equals(e)));
+  }
+
+  @Test
+  public void testGetMostRecentImageDate() throws Exception {
+    LocalDateTime result = surveillanceService.getMostRecentImageDate();
+
+    assertEquals(time1, result);
+  }
+
+  @Test
+  public void testGetMostRecentImageDateForPage() throws Exception {
+    Page<SurveillanceImage> images = surveillanceService.getImagesPage(Optional.of("testcamera1"), 
+        Optional.empty(), false, new PageRequest(0, 10));
+    
+    LocalDateTime result = surveillanceService.getMostRecentImageDate(images, Optional.empty());
+
+    assertEquals(time1, result);
+  }
+
+  @Test
+  public void testGetMostRecentImageDateForPageWithDateFilter() throws Exception {
+    Page<SurveillanceImage> images = surveillanceService.getImagesPage(Optional.of("testcamera1"), 
+        Optional.empty(), false, new PageRequest(0, 10));
+
+    LocalDateTime result = surveillanceService.getMostRecentImageDate(images, Optional.of(LocalDate.now()));
+
+    assertNull(result);
   }
 
 }
