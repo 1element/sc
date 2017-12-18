e1cf45a975e250aae8bde188d6fc3abd697e9041
1element <1element@users.noreply.github.com>
2017-12-18 17:28:17 +0100

    Let ESLint allow dangling underscores for some properties

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -34,6 +34,10 @@ module.exports = {
       'optionalDependencies': ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allow dangling underscore for some properties (hateoas + vue internal)
+    'no-underscore-dangle': ['error', {
+      'allow': ['_embedded', '_links', '_uid']
+    }]
   }
 }
