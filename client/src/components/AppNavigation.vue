50aac3189a427b0ce728163443d0e5e3cfefe47a
1element <1element@users.noreply.github.com>
2017-12-23 13:21:04 +0100

    New navigation layout

--- client/src/components/AppNavigation.vue
+++ client/src/components/AppNavigation.vue
@@ -1,33 +1,28 @@
 <template>
-  <nav v-if="showNavigation" class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <router-link :to="{ name: 'recordings' }" class="navbar-brand">Surveillance Center</router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link :to="{ name: 'recordings' }" class="nav-link">
-            <i class="fa fa-th fa-fw"></i> <span>Recordings</span>
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'liveview' }" class="nav-link">
-            <i class="fa fa-camera fa-fw"></i> <span>Liveview</span>
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'livestream' }" class="nav-link">
-            <i class="fa fa-video-camera fa-fw"></i> <span>Livestream</span>
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'settings' }" class="nav-link">
-            <i class="fa fa-gear fa-fw"></i> <span>Settings</span>
-          </router-link>
-        </li>
-      </ul>
-    </div>
+  <nav v-if="showNavigation" class="navbar navbar-expand navbar-dark fixed-top bg-dark justify-content-center">
+    <div class="navbar-brand d-none d-sm-block pr-3 pt-0">Surveillance Center</div>
+    <ul class="navbar-nav">
+      <li class="nav-item pr-3">
+        <router-link :to="{ name: 'recordings' }" class="nav-link">
+          <i class="material-icons">apps</i>
+        </router-link>
+      </li>
+      <li class="nav-item pr-3">
+        <router-link :to="{ name: 'liveview' }" class="nav-link">
+          <i class="material-icons">photo_camera</i>
+        </router-link>
+      </li>
+      <li class="nav-item pr-3">
+        <router-link :to="{ name: 'livestream' }" class="nav-link">
+          <i class="material-icons">videocam</i>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="{ name: 'settings' }" class="nav-link">
+          <i class="material-icons">settings</i>
+        </router-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
@@ -61,5 +56,15 @@ export default {
 </script>
 
 <style lang="scss">
+  .navbar-dark .navbar-brand {
+    color: rgba(255, 255, 255, 0.75);
 
+    &:hover {
+      color: rgba(255, 255, 255, 0.75);
+    }
+  }
+
+  .router-link-active {
+    color: #fff !important;
+  }
 </style>
